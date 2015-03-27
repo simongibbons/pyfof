b49770e81d4e65857417895173f3fcaaa0673500
Simon Gibbons <simongibbons@gmail.com>
2015-03-27 17:47:27 +0000

    Added keywords and a URL to setup.py

--- setup.py
+++ setup.py
@@ -7,10 +7,12 @@ setup(  name = "pyfof",
         description="Friends-of-friends cluster finding in python",
         author="Simon Gibbons",
         author_email="sljg2@ast.cam.ac.uk",
+        url="https://github.com/simongibbons/pyfof",
+        keywords=['clustering', 'friends-of-friends'],
 
         ext_modules = cythonize(Extension(
             "pyfof",
-            sources=["src/pyfof.pyx", "src/fof.cc"], 
+            sources=["src/pyfof.pyx", "src/fof.cc"],
             extra_compile_args=["-std=c++11"],
             language="c++"
         ))
