5dbe63344f170b8af403c2d4ae922dd7e07ad721
Simon Gibbons <simongibbons@gmail.com>
2015-03-27 17:19:38 +0000

    Updated setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,16 @@ from distutils.core import setup, Extension
 from Cython.Build import cythonize
 
 
-setup( ext_modules = cythonize(Extension(
+setup(  name = "pyfof",
+        version="0.1",
+        description="Friends-of-friends cluster finding in python",
+        author="Simon Gibbons",
+        author_email="sljg2@ast.cam.ac.uk",
+
+        ext_modules = cythonize(Extension(
             "pyfof",
             sources=["src/pyfof.pyx", "src/fof.cc"], 
-            extra_compile_args=["-std=c++11", "-O3"],
+            extra_compile_args=["-std=c++11"],
             language="c++"
-    )))
+        ))
+    )
