cdb0ea337783969d2b0a8b5a176588562e11bb95
Simon Gibbons <simongibbons@gmail.com>
2015-03-27 17:08:39 +0000

    Added documentation for friends_of_friends function

--- src/pyfof.pyx
+++ src/pyfof.pyx
@@ -12,6 +12,15 @@ cdef extern from "fof.hpp":
     cdef list[list[size_t]] _friends_of_friends "friends_of_friends"(double*, size_t, size_t, double)
 
 def friends_of_friends(np.ndarray[double, ndim=2, mode="c"] data, double linking_length):
+    """ Computes friends-of-friends clustering of data. Distances are computed
+    using a euclidian metric.
+
+        :param data: A numpy array with dimensions (npoints x ndim)
+
+        :param linking_length: The linking length between cluster members
+
+        :rtype: A list of lists of indices in each cluster type
+    """
     return _friends_of_friends(&data[0,0],\
                                data.shape[0],\
                                data.shape[1],\
