3e6003f675c6305c7b99c4e187937f791f061f19
Simon Gibbons <simongibbons@gmail.com>
2015-03-30 13:17:09 +0100

    Added code to setup.py to facilitate finding boost include directory

--- setup.py
+++ setup.py
@@ -1,4 +1,9 @@
+import os
+import re
+
 from distutils.core import setup, Extension
+from distutils.command.build_ext import build_ext as _build_ext
+
 import numpy
 
 try:
@@ -8,6 +13,52 @@ except ImportError:
     USE_CYTHON = False
 
 ext = '.pyx' if USE_CYTHON else '.cpp'
+
+def find_boost(hint=None, verbose=True):
+    """ Find the boost include directory,
+    returns ``None`` if not found.
+
+    This is based upon the version in https://github.com/dfm/transit/
+    Copyright (c) 2014 Daniel Foreman-Mackey
+    which is licenced under the MIT licence.
+    """
+
+    search_dirs = [] if hint is None else hint
+    search_dirs += [
+        "/usr/local/include",
+        "/usr/local/homebrew/include",
+        "/opt/local/include",
+        "/data/sljg2/software/boost/include"
+    ]
+
+    print search_dirs
+
+    for d in search_dirs:
+        path = os.path.join(d, "boost", "geometry", "index", "rtree.hpp")
+        if os.path.exists(path):
+            vf = os.path.join(d, "boost", "version.hpp")
+            src = open(vf, "r").read()
+            v = re.findall("#define BOOST_LIB_VERSION \"(.+)\"", src)
+            if not len(v):
+                continue
+            v = v[0]
+            if verbose:
+                print("Found Boost version {0} in: {1}".format(v, d))
+            return d
+    return None
+
+class build_ext(_build_ext):
+    def build_extension(self, ext):
+        dirs = ext.include_dirs + self.compiler.include_dirs
+
+        boost_include = find_boost(hint=dirs)
+        if boost_include is None:
+            raise RuntimeError("Boost not found, see the README for possible solutions")
+
+        ext.include_dirs.append(boost_include)
+
+        _build_ext.build_extension(self, ext)
+
 
 extensions = [Extension(
                 "pyfof",
@@ -28,5 +79,6 @@ setup(  name = "pyfof",
         url="https://github.com/simongibbons/pyfof",
         keywords=['clustering', 'friends-of-friends'],
 
+        cmdclass={'build_ext' : build_ext},
         ext_modules = extensions
     )
