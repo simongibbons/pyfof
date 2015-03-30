c991b615e7d4c5d267e8b59338d909b954781986
Simon Gibbons <simongibbons@gmail.com>
2015-03-30 13:28:16 +0100

    Allowed python to handle exceptions thrown in the c++ code

--- pyfof/pyfof.pyx
+++ pyfof/pyfof.pyx
@@ -14,7 +14,7 @@ cimport numpy as np
 from libcpp.list cimport list
 
 cdef extern from "fof.hpp":
-    cdef list[list[size_t]] _friends_of_friends "friends_of_friends"(double*, size_t, size_t, double)
+    cdef list[list[size_t]] _friends_of_friends "friends_of_friends"(double*, size_t, size_t, double) except +
 
 def friends_of_friends(np.ndarray[double, ndim=2, mode="c"] data, double linking_length):
     """ Computes friends-of-friends clustering of data. Distances are computed
