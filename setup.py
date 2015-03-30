d3f36e43bc705dd60d6f97a42b7ab5ed2afde875
Simon Gibbons <simongibbons@gmail.com>
2015-03-30 14:39:13 +0100

    Bumped version of setup.py

--- setup.py
+++ setup.py
@@ -70,11 +70,12 @@ if USE_CYTHON:
     extensions = cythonize(extensions)
 
 setup(  name = "pyfof",
-        version="0.1-dev",
+        version="0.1.1",
         description="Friends-of-friends cluster finding in python",
         author="Simon Gibbons",
         author_email="sljg2@ast.cam.ac.uk",
         url="https://github.com/simongibbons/pyfof",
+        download_url='https://github.com/simongibbons/pyfof/tarball/v0.1.1',
         license="MIT",
         keywords=['clustering', 'friends-of-friends'],
 
