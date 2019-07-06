64ef0aed11bb1877a442aa3345bc3cf7b82b31ec
Simon Gibbons <simongibbons@gmail.com>
2019-07-06 01:33:54 +0100

    Ensure we get a long description

--- setup.py
+++ setup.py
@@ -70,6 +70,12 @@ class build_ext(_build_ext):
         _build_ext.build_extension(self, ext)
 
 
+# read the contents of your README file
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
 extensions = [Extension(
                 "pyfof",
                 sources=["pyfof/pyfof"+ext, "pyfof/fof.cc"],
@@ -96,5 +102,7 @@ setup(  name = "pyfof",
             'cython',
         ],
         cmdclass={'build_ext' : build_ext},
-        ext_modules = extensions
+        ext_modules = extensions,
+        long_description=long_description,
+        long_description_content_type='text/markdown',
     )
