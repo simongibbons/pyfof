ed8b7bed64a29b32dd4c427c9edf78d914c933bd
Simon Gibbons <simongibbons@gmail.com>
2020-08-08 08:32:57 +0100

    Simplify implementation slightly

--- pyfof/fof.hpp
+++ pyfof/fof.hpp
@@ -3,4 +3,3 @@
 #include <vector>
 
 std::vector< std::vector<size_t> >  friends_of_friends(double* data, size_t npts, size_t ndim, double linking_length);
-
--- pyfof/fof_brute.cc
+++ pyfof/fof_brute.cc
@@ -21,45 +21,39 @@ std::vector< std::vector<size_t> > friends_of_friends_brute(
 	size_t ndim,
 	double linking_length
 ) {
-    typedef std::pair<size_t, double*> Point;
+  
+	typedef std::pair<size_t, double*> Point;
 
     //Create list of unused points with indices
     std::list<Point> unused;
-    std::list< std::list< size_t > > groups;
+    std::vector< std::vector< size_t > > groups;
     for(size_t i=0 ; i<npts ; ++i) {
         unused.push_back(std::make_pair(i, data + i*ndim));
     }
 
     //If there are unused points try to create a new group
     while( unused.size() > 0 ) {
-        std::list<Point> toadd;
+    	auto group = std::vector<size_t>();
+    	auto toadd = std::vector<Point>();
         toadd.push_back(unused.front());
         unused.pop_front();
 
-        //Look through all points found in the group and attempt to 
-        for(auto toadd_it = toadd.begin() ; toadd_it != toadd.end() ; ++toadd_it) {
+        while (!toadd.empty()) {
+        	auto point = toadd.back();
+        	toadd.pop_back();
+        	group.push_back(point.first);
+
             auto unused_it = unused.begin();
             while(unused_it != unused.end()) {
-                if(dist(unused_it->second, toadd_it->second, ndim) < linking_length) {
+                if(dist(unused_it->second, point.second, ndim) < linking_length) {
                     toadd.push_back(*unused_it);
                     unused.erase(unused_it++);
-                } else {
-                    ++unused_it;
-                }
+                }  else {
+                	++unused_it;
+            	}
             }
-        }
-
-        std::list<size_t> group;
-        for(const auto& p : toadd) {
-            group.push_back(p.first);
         }
         groups.push_back(group);
     }
-
-
-    auto result = std::vector<std::vector<size_t>>();
-    for (const auto& group : groups) {
-        result.push_back(std::vector<size_t>(group.cbegin(), group.cend()));
-    }
-    return result;
+    return groups;
 }
